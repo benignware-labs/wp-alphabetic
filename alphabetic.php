9d27d50e51f525107b08ae2f05aa28e0d8e6520c
Rafael Nowrotek <mailbenignware.com>
2019-03-04 14:44:24 +0100

    Bump version

--- alphabetic.php
+++ alphabetic.php
@@ -4,7 +4,7 @@
  Plugin Name: Alphabetic
  Plugin URI: http://github.com/benignware/wp-menu-item-search-form
  Description: Navigate posts alphabetically
- Version: 0.0.1
+ Version: 0.0.2
  Author: Rafael Nowrotek, Benignware
  Author URI: http://benignware.com
  License: MIT
